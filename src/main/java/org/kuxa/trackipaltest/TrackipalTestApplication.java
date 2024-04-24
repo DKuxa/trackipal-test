4cb23b905f5254907f11c608883d29c5ecc472ed
Dima Kuxa <d.kuxa88@gmail.com>
2024-04-24 20:59:54 -0300

    First commit

---
+++ src/main/java/org/kuxa/trackipaltest/TrackipalTestApplication.java
@@ -0,0 +1,13 @@
+package org.kuxa.trackipaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackipalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackipalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kuxa/trackipaltest/TrackipalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kuxa.trackipaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackipalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
