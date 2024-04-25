0d06042ee567c5f5c2240995927155c3fdfbd48c
Dima Kuxa <d.kuxa88@gmail.com>
2024-04-25 11:32:52 -0300

    Package name fix

--- src/main/java/org/kuxa/trackipaltest/service/ShopifyClient.java
+++ src/main/java/org/kuxa/trackipaltest/service/ShopifyClient.java
@@ -1,4 +1,4 @@
-package org.Kuxa.trackipal.service;
+package org.kuxa.trackipaltest.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
